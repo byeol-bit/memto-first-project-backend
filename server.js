952497e8a75ac6017319ba65741be18f31d5f6f9
s576ai <s576ai@gmail.com>
2026-01-31 16:40:08 +0900

    🐛 Fix : 서버 종료 처리 개선. (포트가 자동으로 정리됨)

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const url = require('url');
 const { swaggerUi, specs } = require('./swagger');
 const router = require('./routes/index');
 
+let server;
+
 function start() {
     // let pathname = url.parse(request.url).pathname;
     // if (pathname === '/favicon.ico') {
@@ -13,14 +15,21 @@ function start() {
     app.get('/', (req, res) => {
         res.send('server running!!');
     });
-
+    
     app.use(express.json());
     app.use('/api', swaggerUi.serve, swaggerUi.setup(specs));
     app.use('', router);
 
-    app.listen(3000, () => {
+    server = app.listen(3000, () => {
         console.log('port 3000 is listening');
     });
 }
+
+function close() {
+    server.close(() => process.exit(0))
+}
+
+process.on('SIGINT', close);
+process.on('SIGTERM', close);
 
 module.exports = { start };
