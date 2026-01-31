4caf4a469b66d3356529926b0711f0fcd016600e
s576ai <s576ai@gmail.com>
2026-02-01 01:04:56 +0900

    📄 Docs : 테스트용 주석 제거

--- swagger.js
+++ swagger.js
@@ -23,30 +23,3 @@ module.exports = {
     swaggerUi,
     specs
 };
-
-/**
- * @swagger
- * /api/example:
- *   post:
- *     tags:
- *       - test
- *     summary: 단순 예시1
- *     description: 테스트를 위한 간단한 예시입니다.
- *     parameters:
- *       - in: formData
- *         name: id
- *         type: number
- *         required: true
- *         description: id에 대한 설명, 이것저것 표시됨
- *       - in: formData
- *         name: password
- *     responses:
- *       200:
- *         description: OK
- *         schema:
- *           type: number
- *   get:
- *     tags:
- *       - test
- *     summary: 비어 있는 예시
- */
