d4160b4d4b8a8264f6b8e3305e28ed82471222aa
s576air <s576ai@naver.com>
2026-03-05 15:40:16 +0900

    ✨ Feat : jpg 이미지 사용 가능, 회원가입시 이미지 필수

--- services/userService.js
+++ services/userService.js
@@ -6,7 +6,7 @@ const passwordUtil = require('../utils/password');
 const userRepository = require('../repositories/users');
 const {CATEGORIES, CATEGORIES2, getCategory} = require('../utils/category');
 
-const IMAGE_EXT = ['jpeg', 'png', 'gif', 'webp'];
+const IMAGE_EXT = ['jpg', 'jpeg', 'png', 'gif', 'webp'];
 
 /**
  * @returns {string[]}
@@ -292,21 +292,24 @@ async function updateUser(id, updateUserInput, file) {
 
     let newUpdateUserInput = {};
 
-    if (file) {
-        let ext = file.originalname.split('.').at(-1);
-        if (!IMAGE_EXT.includes(ext) || !file.mimetype.startsWith('image')) {
-            let err = new Error(`파일 타입은 ${IMAGE_EXT}만 허용됩니다.`);
-            err.statusCode = 400;
-            return err;
-        }
-
-        let path = 'images/users';
-        let filename = `${id}.${ext}`;
-
-        await fs.mkdir(path, { recursive: true });
-        await fs.rename(`${file.path}`, `${path}/${filename}`);
-        newUpdateUserInput.profile_image = filename;
+    if (!file) {
+        let err = new Error('이미지가 존재하지 않습니다');
+        err.statusCode = 400;
+        return err;
     }
+    let ext = file.originalname.split('.').at(-1);
+    if (!IMAGE_EXT.includes(ext) || !file.mimetype.startsWith('image')) {
+        let err = new Error(`파일 타입은 ${IMAGE_EXT}만 허용됩니다.`);
+        err.statusCode = 400;
+        return err;
+    }
+
+    let path = 'images/users';
+    let filename = `${id}.${ext}`;
+
+    await fs.mkdir(path, { recursive: true });
+    await fs.rename(`${file.path}`, `${path}/${filename}`);
+    newUpdateUserInput.profile_image = filename;
     
 
     for (const [key, value] of Object.entries(updateUserInput)) {
